a32075f1d3c79542434c59f48b2bedbbd798c658
Ali Mroweh <amroweh@gmail.com>
2024-01-20 23:16:31 +0000

    first commit

---
+++ constants.js
@@ -0,0 +1,8 @@
+export const SHAK_URL = 'http://uat-app.ssdm.bskyb.com:8080/SHAKIRA/'
+export const SHAK_FAIL_MESSAGE = 'Fail :('
+export const SHAK_SUCCESS_MESSAGE = 'Success :)'
+
+export const SHAK_DECACHE_MATCHES = [
+	{url: '/watch/sky-sports-tennis', match: 'resources/decache/allsports/competition/420'},
+	{url: '/watch/sky-sports-football', match: 'resources/decache/allsports/competition/420.11'}
+]
---
+++ popup.js
@@ -0,0 +1,88 @@
+import {SHAK_DECACHE_MATCHES, SHAK_FAIL_MESSAGE, SHAK_SUCCESS_MESSAGE, SHAK_URL} from './constants.js'
+
+let shakDecacheButton,
+	shakDecacheButtonSpinner,
+	shakDecacheMessage,
+	shakDecacheMessageResult,
+	shakDecacheMessageContent = null
+
+document.addEventListener('DOMContentLoaded', function () {
+	shakDecacheButton = document.querySelector('#shak_decache_button')
+	shakDecacheButtonSpinner = document.querySelector('#shak_decache_button_spinner')
+	shakDecacheMessage = document.querySelector('#shak_decache_message')
+	shakDecacheMessageResult = document.querySelector('#shak_decache_message_result')
+	shakDecacheMessageContent = document.querySelector('#shak_decache_message_content')
+
+	shakDecacheButton.addEventListener('click', () => {
+		clearShakDecacheMessage()
+		hideShakDecacheMessage()
+		showSpinner()
+		printCurrentURL()
+	})
+})
+
+const printCurrentURL = async () => {
+	chrome.tabs.query({active: true, currentWindow: true}, async tabs => {
+		const url = tabs[0].url
+		const urlMatch = matchPath(getURLParts(url).pathname)
+		// CASE 1: Couldn't find a match
+		if (!urlMatch) failFetch('No Match for this URL Path')
+		// CASE 2: Found a match and initiating decache request
+		let res = null
+		try {
+			res = await fetch(SHAK_URL + urlMatch, {
+				headers: {
+					Accept: 'application/json'
+				}
+			})
+		} catch (e) {
+			// CASE 3: Decache request had an issue (could be because of unconnected VPN)
+			return failFetch('Unable to fetch from SHAK. Are you connected to the VPN?')
+		}
+		const response = await res.json()
+		// CASE 4: Request sent to SHAK but issue on the server side
+		if (response.message) {
+			return failFetch(response.message)
+		}
+		// CASE 5: Success :)
+		else successFetch(response['@text'])
+	})
+}
+
+// DOM Functions
+const showSpinner = () => shakDecacheButtonSpinner.classList.add('active')
+const hideSpinner = () => shakDecacheButtonSpinner.classList.remove('active')
+const clearShakDecacheMessage = () => (shakDecacheMessage.innerHtml = '')
+const hideShakDecacheMessage = () => shakDecacheMessage.classList.add('hide')
+const addShakDecacheMessage = message => (shakDecacheMessageContent.innerHTML = message)
+const addShakDecacheMessageResult = result => (shakDecacheMessageResult.innerHTML = result)
+
+// Utility Functions
+const getURLParts = url => {
+	const myURL = new URL(url)
+	return {
+		origin: myURL.origin,
+		hostname: myURL.hostname,
+		port: myURL.port,
+		pathname: myURL.pathname,
+		href: myURL.href
+	}
+}
+
+const failFetch = message => {
+	addShakDecacheMessageResult(SHAK_FAIL_MESSAGE)
+	addShakDecacheMessage(message)
+	shakDecacheMessage.classList.remove('hide', 'success')
+	shakDecacheMessage.classList.add('fail')
+	hideSpinner()
+}
+
+const successFetch = message => {
+	addShakDecacheMessageResult(SHAK_SUCCESS_MESSAGE)
+	addShakDecacheMessage(message)
+	shakDecacheMessage.classList.remove('hide', 'fail')
+	shakDecacheMessage.classList.add('success')
+	hideSpinner()
+}
+
+const matchPath = pathname => SHAK_DECACHE_MATCHES.find(entry => entry.url === pathname)?.match

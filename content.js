962bd2196dea167bb2c26be91c0214bfe273899c
Ali Mroweh <amroweh@gmail.com>
2024-01-26 18:50:46 +0000

    added ability to show HEXA/Legacy

--- content.js
+++ content.js
@@ -1,4 +1,25 @@
-console.log('hello from content script')
+console.log('CONTENT SCRIPT LOADED!!!')
+console.log(document.querySelector('html').classList)
+const generatePopup = isHexa => {
+	const popup = document.createElement('span')
+	popup.classList.add('sky_toolbox_popup')
+	popup.textContent = isHexa ? 'HEXA' : 'LEGACY'
+	popup.style.backgroundColor = isHexa ? 'blue' : 'purple'
+	popup.style.color = 'white'
+	popup.style.fontWeight = 'bold'
+	popup.style.padding = '10px 15px'
+	popup.style.position = 'fixed'
+	popup.style.zIndex = 1000000
+	popup.style.top = 0
+	popup.style.right = 0
+	return popup
+}
+
+const addPopup = () => {
+	if (window.location.host !== 'www.skysports.com') return
+	document.querySelector('body').append(generatePopup(document.querySelector('html').classList.contains('is-modern')))
+}
+addPopup()
 
 const injectIntoTicket = content => {
 	const ticketBody = document

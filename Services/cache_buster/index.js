ebd18a9ab4d98a8836a74590bbd4f0acd74a556d
Ali Mroweh <amroweh@gmail.com>
2024-04-26 19:13:02 +0100

    cache buster now works with existing url params

--- Services/cache_buster/index.js
+++ Services/cache_buster/index.js
@@ -9,16 +9,26 @@ let cacheBusterButton, cacheBusterButtonSpinner
 const decacheCurrentURL = async () => {
 	chrome.tabs.query({active: true, currentWindow: true}, async tabs => {
 		const previousUrl = tabs[0].url
-		const bustedUrl = appendToUrl(removeTrailingSlash(removeExistingCacheBuster(previousUrl)))
+		const bustedUrl = generateNewUrl(previousUrl)
 		chrome.tabs.update({url: bustedUrl})
 	})
 }
 
 // Utility Functions
-const removeExistingCacheBuster = url => {
-	const cbIndex = url.indexOf('?cb')
-	if (cbIndex > -1) return url.slice(0, cbIndex)
-	return url
-}
 const removeTrailingSlash = url => (url.endsWith('/') ? url.slice(0, -1) : url)
-const appendToUrl = url => url + '?cb=' + Date.now()
+const generateNewUrl = url => {
+	const cbIndexH = url.indexOf('&cb')
+	if (cbIndexH > -1) {
+		return appendToUrl(removeTrailingSlash(url.slice(0, cbIndexH)), '&')
+	}
+	const cbIndexQ = url.indexOf('?cb')
+	if (cbIndexQ > -1) {
+		return appendToUrl(removeTrailingSlash(url.slice(0, cbIndexQ)), '?')
+	}
+	const indexQ = url.indexOf('?')
+	if (indexQ > -1) {
+		return appendToUrl(removeTrailingSlash(url), '&')
+	}
+	return appendToUrl(removeTrailingSlash(url), '?')
+}
+const appendToUrl = (url, tag) => url + tag + 'cb=' + Date.now()

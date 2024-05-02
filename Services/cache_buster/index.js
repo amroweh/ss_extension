e0eb100d1ee2bc19094fa457908d13464f0597cf
Ali Mroweh <amroweh@gmail.com>
2024-05-02 23:58:51 +0100

    cache buster now works when existing buster is between other query params

--- Services/cache_buster/index.js
+++ Services/cache_buster/index.js
@@ -17,18 +17,28 @@ const decacheCurrentURL = async () => {
 // Utility Functions
 const removeTrailingSlash = url => (url.endsWith('/') ? url.slice(0, -1) : url)
 const generateNewUrl = url => {
-	const cbIndexH = url.indexOf('&cb')
-	if (cbIndexH > -1) {
-		return appendToUrl(removeTrailingSlash(url.slice(0, cbIndexH)), '&')
+	// cache buster exists and is not at the start
+	const cbIndexA = url.indexOf('&cb')
+	if (cbIndexA > -1) {
+		const indexOfLastDigit = getIndexOfLastDigitInCacheBuster(url, cbIndexA)
+		const urlPre = url.slice(0, cbIndexA)
+		const urlPost = url.slice(indexOfLastDigit)
+		const finalUrl = appendToUrl(removeTrailingSlash(urlPre), '&') + urlPost
+		return finalUrl
 	}
+	// cache buster exists and is at the start
 	const cbIndexQ = url.indexOf('?cb')
-	if (cbIndexQ > -1) {
-		return appendToUrl(removeTrailingSlash(url.slice(0, cbIndexQ)), '?')
-	}
+	if (cbIndexQ > -1) return appendToUrl(removeTrailingSlash(url.slice(0, cbIndexQ)), '?')
+	// cache buster doesn't exist and there are query params
 	const indexQ = url.indexOf('?')
-	if (indexQ > -1) {
-		return appendToUrl(removeTrailingSlash(url), '&')
-	}
+	if (indexQ > -1) return appendToUrl(removeTrailingSlash(url), '&')
+	// cache buster doesn't exist and there are no query params
 	return appendToUrl(removeTrailingSlash(url), '?')
 }
+
 const appendToUrl = (url, tag) => url + tag + 'cb=' + Date.now()
+const getIndexOfLastDigitInCacheBuster = (url, cbIndex) => {
+	let i = cbIndex + 4
+	while (url[i] !== undefined && url[i] !== null && url[i] < 10) i++
+	return i
+}
